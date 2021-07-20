94023a5984af1083e329bbffc4a04a31af67d20b
Rayanne B. Lima <rayannelima@teksystem.com.br>
2021-07-20 08:50:46 -0300

    Aula 05 - Configurações Finais e Firebase Deploy

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from 'react';
+import cx from 'classnames';
+
 import '../styles/question.scss';
 
 type QuestionProps = {
@@ -7,13 +9,21 @@ type QuestionProps = {
     name: string;
     avatar: string;
   };
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
   children?: ReactNode;
 }
 
-export function Question ({ content, author, children }: QuestionProps) {
+export function Question ({ content, author, isAnswered = false, isHighlighted = false, children }: QuestionProps) {
 
   return (
-    <div className="question">
+    <div
+      className={ cx(
+        'question',
+        {answered : isAnswered},
+        {highlighted : isHighlighted && !isAnswered}
+      )}
+    >
       <p>{ content }</p>
       <footer>
         <div className="user-info">
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -5,8 +5,9 @@ import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
 import logoImg from '../assets/images/logo.svg';
-import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
 import answerImg from '../assets/images/answer.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -30,6 +31,14 @@ export function AdminRoom() {
     await database.ref(`rooms/${roomId}`).update({ closedAt: new Date() });
 
     history.push('/');
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({ isAnswered: true });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({ isHighlighted: true });
   }
 
   async function handleDeleteQuestion(questionId: string) {
@@ -68,16 +77,32 @@ export function AdminRoom() {
                   key={question.id}
                   content={question.content}
                   author={question.author}
+                  isAnswered={question.isAnswered}
+                  isHighlighted={question.isHighlighted}
                 >
-                  <div>
-                    <button>
-                      <img src={answerImg} alt="Remover pergunta" />
-                    </button>
+                  <div className="admin-buttons">
+                    { !question.isAnswered
+                      && (
+                      <>
+                        <button
+                          type="button"
+                          onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                        >
+                          <img src={checkImg} alt="Marcar pergunta como respondida" />
+                        </button>
+                        <button
+                          type="button"
+                          onClick={() => handleHighlightQuestion(question.id)}
+                        >
+                          <img src={answerImg} alt="Dar destaque à pergunta" />
+                        </button>
+                      </>
+                    )}
                     <button
                       type="button"
                       onClick={() => handleDeleteQuestion(question.id)}
                     >
-                      <img src={deleteImg} alt="" />
+                      <img src={deleteImg} alt="Remover pergunta" />
                     </button>
                   </div>
                 </Question>
